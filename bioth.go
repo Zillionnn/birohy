7c811aa4e532553e0acad25a994648d9486a2232
Administrator <skyestzhang@163.com>
2019-11-21 19:13:55 +0800

    use go

--- bioth.go
+++ bioth.go
@@ -1,10 +1,43 @@
 package main
 
-import("fmt")
+import(
+	"fmt"
+	"time"
+)
 
-func getDaysFromBirth(birthday, date){
-	
+var bodyState map[string]string
+bodyState = make(map[string]string)
+bodyState["HIGH"]= "HIGH PERIOD"
+bodyState["LOW"]= "LOW PERIOD"
+bodyState["CRITICAL"]= "WARNING, IN CRITICAL DAY!"
+
+
+func getDaysFromBirth(birthday string, date string) int64{
+	m_birthday,_ := time.Parse("01/02/2006", birthday);
+	m_date, _ :=time.Parse("01/02/2006",date);
+	duration := (m_date.Unix() - m_birthday.Unix()) / 60/60/24
+	return	duration
 }
+
+func formatState(type string, days int) string{
+	s:=''
+	if type=="phsical" {
+		if days<12 {
+			s+= bodyState["HIGH"]
+		} else if days ==12 {
+			s+=bodyState["CRITICAL"]
+		} else {
+			s+=bodyState["LOW"]
+		}
+	}
+	return s
+}
+
 func main()  {
-	fmt.Println("234")
+	duration := getDaysFromBirth("08/07/1993", "11/21/2019")
+	phiscal := duration  % 23
+	emotion := duration %28
+	inte:=duration%33
+
+	fmt.Println(formatState("phiscal", phiscal));
 }
